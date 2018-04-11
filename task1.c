a921bf4b72821cd426be0034ee405cc30fdf4fec
Michael <michael.kuechler2@uzh.ch>
2018-04-11 15:41:20 +0200

    empty functions

--- task1.c
+++ task1.c
@@ -1,6 +1,26 @@
 //Michael Küchler, 16-924-318
 
-#include stdio.h
+#include <stdio.h>
+
+struct Node{
+	int key;
+	struct Node* next;
+};
+
+int isEmpty(struct Node* stackTop){
+	
+	return 0;
+}
+void push(struct Node** stackTop,int key){
+	
+}
+int pop(struct Node** stackTop,int key){
+	
+	return 0;
+}
+void printStack(struct Node* stackTop){
+	
+}
 
 
 int main(){
