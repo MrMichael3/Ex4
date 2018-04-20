0b663445a0791ccef6c62f46bbed56090935eea0
Michael <michael.kuechler2@uzh.ch>
2018-04-20 17:03:15 +0200

    finished

--- task2.c
+++ task2.c
@@ -8,6 +8,11 @@ struct Node{
 	struct Node* next;
 };
 
+struct Queue{
+	struct Node* stack1;
+	struct Node* stack2;
+};
+
 int isEmpty(struct Node* stackTop){
 	if(stackTop == NULL){
 		return 1;
@@ -32,7 +37,6 @@ int pop(struct Node** stackTop){
 	return key;
 }
 void printStack(struct Node* stackTop){
-	printf("stack:\n");
 	if(isEmpty(stackTop)==0){
 		while(stackTop->next != NULL){
 			printf("%d\n",stackTop->key);
@@ -43,12 +47,59 @@ void printStack(struct Node* stackTop){
 	}	
 }
 
-struct Queue{
-	struct Node* stack1;
-	struct Node* stack2;
-};
 
+
+void enQueue(struct Queue* q, int key){
+	push(&q->stack1,key);
+}
+
+int deQueue(struct Queue* q){
+	//empty stack
+	if(isEmpty(q->stack1)){
+		return -1;
+	}
+	//from stack 1 to stack 2
+	int value;
+	while(isEmpty(q->stack1)==0){
+		value = pop(&q->stack1);
+		push(&q->stack2,value);
+	}
+	//key to return
+	int key = pop(&q->stack2);
+	//from stack 2 back to 1
+	while(isEmpty(q->stack2)==0){
+		value = pop(&q->stack2);
+		push(&q->stack1,value);
+	}
+	return key;
+}
+void printQueue(struct Queue* q){
+	printf("Queue:\n");
+	int value;
+	//change order
+	while(isEmpty(q->stack1)==0){
+		value = pop(&q->stack1);
+		push(&q->stack2,value);
+	}
+	printStack(q->stack2);
+	
+	
+	
+	
+}
 int main(){
+	struct Queue* q = malloc(sizeof(struct Queue));
+	q->stack1 = NULL;
+	q->stack2 = NULL;
+	enQueue(q,2);
+	enQueue(q,8);
+	enQueue(q,11);
+	enQueue(q,15);
+	deQueue(q);
+	deQueue(q);
+	enQueue(q,1);
+	enQueue(q,5);
+	printQueue(q);
 	
 }
 
