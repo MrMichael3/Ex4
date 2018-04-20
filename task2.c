274a0d3d5693fa23e491cb6e6a980a9bffff6b86
Michael <michael.kuechler2@uzh.ch>
2018-04-20 16:06:53 +0200

    added from task 1

--- task2.c
+++ task2.c
@@ -1,8 +1,55 @@
 //Michael Küchler, 16-924-318
 
-#include stdio.h
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Node{
+	int key;
+	struct Node* next;
+};
 
-int main(){
+int isEmpty(struct Node* stackTop){
+	if(stackTop == NULL){
+		return 1;
+	}
 	return 0;
 }
+void push(struct Node** stackTop,int key){
+	struct Node* newNode = malloc(sizeof(struct Node));
+	newNode->key=key;
+	newNode->next=*stackTop;
+	*stackTop=newNode;
+	
+}
+int pop(struct Node** stackTop){
+	if(isEmpty(*stackTop)){
+		return -1;
+	}
+	int key = (*stackTop)->key;
+	struct Node *temp = (*stackTop)->next;
+	free(*stackTop);
+	*stackTop=temp;
+	return key;
+}
+void printStack(struct Node* stackTop){
+	printf("stack:\n");
+	if(isEmpty(stackTop)==0){
+		while(stackTop->next != NULL){
+			printf("%d\n",stackTop->key);
+			stackTop = stackTop->next;
+		}
+		//print first element
+		printf("%d\n",stackTop->key);
+	}	
+}
+
+struct Queue{
+	struct Node* stack1;
+	struct Node* stack2;
+};
+
+int main(){
+	
+}
+
+
